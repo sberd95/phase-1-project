1bb961fe33d2abf039fc7ca40563f1397b2f819a
Sergio Berdeja <sergioberdeja@gmail.com>
2022-05-21 16:53:06 -0600

    Project completed

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-A fun little trivia website.
+# Trivia Night: The Game!
+Trivia Night is a single page application intended to communicate with the [Open Trivia DB API](https://opentdb.com/). The application features a form where users can specify the parameters of their call to the API to customize their quiz.
+
+---
+
+### Quiz options include:
+- Difficulty Level
+- Question Category
+- Type of Question (*True/False or Multiple Choice*)
+- Number of Questions Desired
+
+##### Please note if your quiz does not immediately start after submission, it is likely because you requested more questions than the database has available for your parameters.
+
+---
+## Quick-Start Functionality
+Trivia Night also features a **Quick-Start** button that allows more *time-constrained* users to jump right into a random quiz of 10 questions. For this API call, the default parameters of Any are invoked for the category, difficulty, and question type. 
+
+## Quality of Life Enhancements
+When you select your answer in Trivia Night and submit to lock it in, a color-coded response is given to indicate the status of the answer you picked. Additionally, if you failed to select an answer, an alert will appear to let you know so.
+
+When you reach the end of your quiz, you are presented with a special button that will both display your final score *and* reintroduce the quiz menu if you wish to play again. Depending on the score you attain, one of ***three*** possible end messages will appear either informing you, congratulating you or *really* congratulating you on your efforts.
+
+### Final Note
+Big thanks to the Open Trivia DB API for making this project possible and I hope you enjoy Trivia Night. Additional features may come in the future.  
--- index.js
+++ index.js
@@ -4,74 +4,67 @@ const multiB = document.getElementById('optionB')
 const multiC = document.getElementById('optionC')
 const multiD = document.getElementById('optionD')
 
-const radioA = document.getElementById('radioA')
-const radioB = document.getElementById('radioB')
-const radioC = document.getElementById('radioC')
-const radioD = document.getElementById('radioD')
-
-const boolT = document.getElementById('optionT')
-const boolF = document.getElementById('optionF')
-
-const radioT = document.getElementById('radioT')
-const radioF = document.getElementById('radioF')
-
 const formMulti = document.getElementById('questionMulti')
 const formBool = document.getElementById('questionTF')
 
-//upon multiple choice submission, checks radio buttons one by one and checks the active one against the correct answer to give a result
-document.querySelector('#questionMulti').addEventListener('submit', e => {
-    e.preventDefault()
-    const radioButtons = document.querySelectorAll('input[name="radioMulti"]')
-    console.log('radio button collection: ', radioButtons)
-    for (const btn of radioButtons) {
-        console.log('current radio button: ', btn, btn.checked)
-        if (btn.checked) {
-            debugger
-            console.log('siblings of the current button: ', btn.parentNode.childNodes)
-            console.log('selected: ', btn.parentNode.children[0].innerText, 'answer: ', qCorrect )
-            
-            document.getElementById('submitMulti').classList.remove('d-block')
-            document.getElementById('submitMulti').classList.add('d-none')
-            if (btn.parentNode.children[0].innerText === qCorrect) {
-                return correctAnswer()
-            }
-            else {
-                return wrongAnswer()
-            }
-        }
-    }
-    return errorMessage()
-})
+const qCat = document.getElementById('category')
+const qDiff = document.getElementById('difficulty')
+const qType = document.getElementById('qType')
+const qNum = document.getElementById('numQuestions')
 
-document.querySelector('#questionTF').addEventListener('submit', e => {
-    e.preventDefault()
-    if (radioT.checked) {
-        document.getElementById('submitTF').classList.remove('d-block')
-        document.getElementById('submitTF').classList.add('d-none')
-        if (boolT.innerText === qCorrect) {
-            correctAnswer()
+//initializing variables to track questions and associated data out of function scopes
+let questionArr, questionRem, questionTot, qCorrect, qScore
+
+//Upon page loading, we will populate the categories field in the form using a special API request
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://opentdb.com/api_category.php')
+    .then(resp => resp.json())
+    .then(list => {
+        for (const category of list.trivia_categories) {
+            let newCat = document.createElement('option')
+            newCat.textContent = category.name
+            newCat.value = category.id
+            qCat.appendChild(newCat)
         }
-        else {
-            wrongAnswer()
-        }
-    }   
-    else if (radioF.checked) {
-        document.getElementById('submitTF').classList.remove('d-block')
-        document.getElementById('submitTF').classList.add('d-none')
-        if (boolF.innerText === qCorrect) {
-            correctAnswer()
-        }
-        else {
-            wrongAnswer()
-        }
-    }
-    else {
-        errorMessage()
-    }
+    })
 })
 
 //TESTING button, will serve as quick-start for user
-document.querySelector('#questionSkip').addEventListener('click', questionGetter)
+document.querySelector('#questionSkip').addEventListener('click', () => questionGetter())
+//Full submit form conversion to request URL for API
+document.querySelector('#quizmaker').addEventListener('submit', event => {
+    event.preventDefault()
+    debugger
+    let urlString = ''
+    urlString += 'amount=' + qNum.value
+    questionRem = parseInt(qNum.value) 
+    questionTot = parseInt(qNum.value)
+
+    urlString += '&category=' + qCat.value
+    urlString += '&difficulty=' + qDiff.value
+    urlString += '&type=' + qType.value
+    questionGetter(urlString)
+})
+
+//this promise took too long to resolve without async functionality to hand to quizGiver
+async function questionGetter(urlString = 'amount=10') {
+    // console.log(urlString)
+    await fetch(`https://opentdb.com/api.php?${urlString}`)
+    .then(resp => resp.json())
+    .then(obj => {
+        if (obj.response_code === 1) {
+            return alert('Not enough questions found, try choosing less questions.')
+        }
+        questionArr = obj.results})
+    qScore = 0
+    //this section will work with the quick-start feature
+    if (urlString === 'amount=10') {
+        questionRem = 10
+        questionTot = 10
+    }
+    document.querySelector('header').classList.toggle('d-none')
+    quizGiver()
+}
 
 //Next question button
 document.getElementById('nextQuestion').addEventListener('click', e => {
@@ -87,29 +80,25 @@ document.getElementById('getResults').addEventListener('click', e => {
     document.getElementById('deleteMe').remove()
     quizGiver()
 })
-//initializing variables to track questions and associated data out of function scopes
-let questionArr, questionRem, questionTot, qCorrect, qScore
-
-//This function gets the questions from the API, no addl params for now
-//this promise took too long to resolve without async functionality to hand to quizGiver
-async function questionGetter(amount = 10) {
-    await fetch('https://opentdb.com/api.php?amount=10')
-    .then(resp => resp.json())
-    .then(obj => {questionArr = obj.results})
-    //this will be 10 for now, to avoid an event error later
-    questionRem = 10
-    questionTot = 10
-    qScore = 0
-    quizGiver()
+//Function to handle what button should be displayed after a question
+function endQButton() {
+    if (questionRem > 0) {
+        document.getElementById('nextQuestion').classList.add('d-block')
+        document.getElementById('nextQuestion').classList.remove('d-none')
+        return
+    }
+    document.getElementById('getResults').classList.add('d-block')
+    document.getElementById('getResults').classList.remove('d-none')
 }
 
+
 //This function will map an array question item onto the buttons
-function quizGiver(questions = 10) {
+function quizGiver() {
     formBool.reset()
     formMulti.reset()
     if (questionRem > 0) {
         document.getElementById('currentQuestion').classList.remove('d-none')
-        console.log(questionArr)
+        // console.log(questionArr)
         let q = questionArr[questionRem-1]
         //getting the right answer out of the scope and setting the new question
         qCorrect = decodeHTML(q.correct_answer)
@@ -120,7 +109,7 @@ function quizGiver(questions = 10) {
             multiB.innerText = decodeHTML(mixedArray[1])
             multiC.innerText = decodeHTML(mixedArray[2])
             multiD.innerText = decodeHTML(mixedArray[3])
-
+            
             formMulti.classList.remove('d-none')
             formBool.classList.add('d-none')
             //bootstrap CSS causes problems if both d-tags are left on
@@ -130,7 +119,7 @@ function quizGiver(questions = 10) {
         else if (q.type === 'boolean') {
             formMulti.classList.add('d-none')
             formBool.classList.remove('d-none')
-
+            
             document.getElementById('submitTF').classList.add('d-block')
             document.getElementById('submitTF').classList.remove('d-none')
         }
@@ -143,16 +132,17 @@ function quizGiver(questions = 10) {
     formBool.classList.add('d-none')
     formMulti.classList.add('d-none')
     document.getElementById('currentQuestion').classList.add('d-none')
-
+    //different messages for score thresholds
     if (qScore === questionTot) {
         document.querySelector('#numQuestion').innerText = `You got a perfect score! Awesome!`
     }
-    else if (qScore/questions >= 0.7) {
+    else if (qScore/questionTot >= 0.7) {
         document.querySelector('#numQuestion').innerText = `You got a total of ${qScore} out of ${questionTot}!`
     }
     else {
         document.querySelector('#numQuestion').innerText = `You got a total of ${qScore} out of ${questionTot}.`
     }
+    document.querySelector('header').classList.toggle('d-none')
 }
 
 //method i found on stackoverflow to shuffle array entries with each other
@@ -167,45 +157,99 @@ function questionShuffle(questionObject) {
     }
     return newQArray
 }
-
 //piece of code i found on stackoverflow to decode html from the API
 function decodeHTML(string) {
     const text = document.createElement('textarea')
     text.innerHTML = string
     return text.value
 }
+
+//upon multiple choice submission, checks radio buttons one by one and checks the active one against the correct answer to give a result
+document.querySelector('#questionMulti').addEventListener('submit', e => {
+    e.preventDefault()
+    const radioButtons = document.querySelectorAll('input[name="radioMulti"]')
+
+    for (const btn of radioButtons) {
+        if (btn.checked) {
+            //upon finding a checked button, the submit button will disappear to prevent glitches
+            document.getElementById('submitMulti').classList.remove('d-block')
+            document.getElementById('submitMulti').classList.add('d-none')
+            if (btn.parentNode.children[0].innerText === qCorrect) {
+                //upon correct answer confirmation, the selection will flash green for a second
+                btn.parentNode.classList.remove('nice-yellow')
+                btn.parentNode.classList.add('green-correct')
+                setTimeout(() => {
+                    btn.parentNode.classList.remove('green-correct')
+                    btn.parentNode.classList.add('nice-yellow')
+                }, 1000)
+                return correctAnswer()
+            }
+            else {
+                //upon incorrect answer, the selection will flash red for a second
+                btn.parentNode.classList.remove('nice-yellow')
+                btn.parentNode.classList.add('red-incorrect')
+                setTimeout(() => {
+                    btn.parentNode.classList.remove('red-incorrect')
+                    btn.parentNode.classList.add('nice-yellow')
+                }, 1000)
+                return wrongAnswer()
+            }
+        }
+    }
+    //should trigger this return if no button was selected
+    return errorMessage()
+})
+//listener for corresponding True/False question submission, same method as Multi
+document.querySelector('#questionTF').addEventListener('submit', e => {
+    e.preventDefault()
+    const radioButtons = document.querySelectorAll('input[name="radioTF"]')
+
+    for (const btn of radioButtons) {
+        if (btn.checked) {            
+            document.getElementById('submitTF').classList.remove('d-block')
+            document.getElementById('submitTF').classList.add('d-none')
+            if (btn.parentNode.children[0].innerText === qCorrect) {
+                btn.parentNode.classList.remove('nice-yellow')
+                btn.parentNode.classList.add('green-correct')
+                setTimeout(() => {
+                    btn.parentNode.classList.remove('green-correct')
+                    btn.parentNode.classList.add('nice-yellow')
+                }, 1000)
+                return correctAnswer()
+            }
+            else {
+                btn.parentNode.classList.remove('nice-yellow')
+                btn.parentNode.classList.add('red-incorrect')
+                setTimeout(() => {
+                    btn.parentNode.classList.remove('red-incorrect')
+                    btn.parentNode.classList.add('nice-yellow')
+                }, 1000)
+                return wrongAnswer()
+            }
+        }
+    }
+    return errorMessage()
+})
+
 //set of functions to handle correct/incorrect answer and move on to the next question
 function correctAnswer() {
     qScore++
-    const congrats = document.createElement('p')
+    const congrats = document.createElement('div')
     congrats.innerText = 'CORRECT!!!'
-    congrats.style.color = 'green'
     congrats.id = 'deleteMe'
-    congrats.classList.add('d-block', 'w-25', 'text-center', 'm-auto')
+    congrats.classList.add('d-block', 'w-25', 'text-center', 'm-auto', 'green-correct')
     document.querySelector('#answerMessage').appendChild(congrats)
     endQButton()
 }
 function wrongAnswer() {
-    const wahwah = document.createElement('p')
+    const wahwah = document.createElement('div')
     wahwah.innerText = 'Sorry, not quite.'
-    wahwah.style.color = 'red'
     wahwah.id = 'deleteMe'
-    wahwah.classList.add('d-block', 'w-25', 'text-center', 'm-auto')
+    wahwah.classList.add('d-block', 'w-25', 'text-center', 'm-auto', 'red-incorrect')
     document.querySelector('#answerMessage').appendChild(wahwah)
     endQButton()
 }
 //error message, probably only goes off in case nothing's selected... for now
 function errorMessage() {
     alert("Did you forget to select an answer?")
-}
-
-//Function to handle what button should be displayed after a question
-function endQButton() {
-    if (questionRem > 0) {
-        document.getElementById('nextQuestion').classList.add('d-block')
-        document.getElementById('nextQuestion').classList.remove('d-none')
-        return
-    }
-    document.getElementById('getResults').classList.add('d-block')
-    document.getElementById('getResults').classList.remove('d-none')
 }
